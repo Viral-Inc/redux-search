6a0a7c8ddbfd34386d642ee1e518c8bc231c34cc
Dillon <d@viral.net>
2017-07-13 15:11:47 -0400

    x

--- website/createAppStore.js
+++ website/createAppStore.js
@@ -4,8 +4,11 @@ import { reducer as searchReducer, reduxSearch } from '../source/index'
 import { reducer as resourceReducer } from './resources'
 import thunk from 'redux-thunk'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+  compose;
+
 export default function createAppStore (): Object {
-  const finalCreateStore = compose(
+  const finalCreateStore = composeEnhancers(
     applyMiddleware(thunk),
     reduxSearch({
       resourceIndexes: {
